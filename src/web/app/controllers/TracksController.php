b31c953e70264899db04625720eb28b21662bf83
Anthony Pernot <anthony.pernot@hotmail.fr>
2020-01-30 18:54:06 +0100

    Autorisation CSRF formulaire import de fichier

--- src/web/app/controllers/TracksController.php
+++ src/web/app/controllers/TracksController.php
@@ -74,7 +74,7 @@ class TracksController extends Controller{
     function moveUploadedFile($directory, UploadedFileInterface $uploadedFile)
     {
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
-        $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+        $basename = bin2hex(random_bytes(8));
         $filename = sprintf('%s.%0.8s', $basename, $extension);
 
         $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
