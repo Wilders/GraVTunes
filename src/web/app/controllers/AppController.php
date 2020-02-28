19635404acbf327c075e69c8df41b480059d7d43
PERNOT Anthony <anthony.pernot@hotmail.fr>
2020-02-28 13:24:14 +0100

    Affichage des stats sur le tableau de bord de l'utilisateur

--- src/web/app/controllers/AppController.php
+++ src/web/app/controllers/AppController.php
@@ -2,6 +2,10 @@
 
 namespace app\controllers;
 
+use app\helpers\Auth;
+use app\models\Commande;
+use app\models\Ticket;
+use app\models\Track;
 use Braintree_ClientToken;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -13,7 +17,16 @@ use Slim\Http\Response;
 class AppController extends Controller {
 
     public function showHome(Request $request, Response $response, array $args): Response {
-        $this->view->render($response, 'pages/home.twig');
+        $user = Auth::user();
+        $tracksUser = Track::where('user_id','=', $user->id)->get();
+        $commandesUser = Commande::where('user_id','=',$user->id)->get();
+        $ticketsUser = Ticket::where(['user_id' => $user->id, 'statut' => 0])->get();
+        $this->view->render($response, 'pages/home.twig',[
+            "user" => $user,
+            "tracks" => $tracksUser,
+            "commandes" => $commandesUser,
+            "tickets" => $ticketsUser
+        ]);
         return $response;
     }
 
--- src/web/app/controllers/HomeController.php
+++ src/web/app/controllers/HomeController.php
@@ -3,10 +3,6 @@
 namespace app\controllers;
 
 use app\helpers\Auth;
-use app\models\Commande;
-use app\models\Message;
-use app\models\Ticket;
-use app\models\Track;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -17,17 +13,8 @@ use Slim\Http\Response;
 class HomeController extends Controller {
 
     public function showHome(Request $request, Response $response, array $args): Response {
-        $user = Auth::user();
-        $tracksUser = Track::where('user_id','=',$user->id)->count();
-        $commandesUser = Commande::where('user_id','=',$user->id)->get();
-        $messagesUser = Message::where('user_id','=',$user->id)->get();
         if(Auth::check()) {
-            $response = $response->withRedirect($this->router->pathFor('appHome',[
-               "user" => $user,
-               "tracks" => $tracksUser,
-               "commandes" => $commandesUser,
-               "messages" => $messagesUser
-            ]));
+            $response = $response->withRedirect($this->router->pathFor('appHome'));
         } else {
             $response = $response->withRedirect($this->router->pathFor('login'));
         }
